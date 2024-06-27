5c234481f998fe8bf0aff06e6de0f05261baf123
Miguel-Angel Sanchez Ndoye <miguel.ngl.sanchez@gmail.com>
2024-06-27 19:16:21 +0200

    added a script for tables of contents and started working on the git cheat sheet

--- script.js
+++ script.js
@@ -58,3 +58,16 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+
